d632b64816c149ad3b74c64b1e9141235449a2ae
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-15 19:50:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://api.darksky.net/forecast/9ec3aff31bf59b89c5dd23c9325ab439/42.3601,-71.0589', true)
 
 request.onload = function () {
   // Begin accessing JSON data here
