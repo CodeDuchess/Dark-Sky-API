dd3b515e0a6cce42cabaebe64e6b05800b8a023a
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-15 20:17:08 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
+
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
@@ -16,21 +16,29 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-      Object.keys(data).forEach(movie => {
+      Object.keys(data).forEach(weather => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = weather.timezone;
 
+      const h2 = document.createElement('h2');
+      h2.textContent = weather.temperature;  
+        
+      const h3 = document.createElement('h3);
+      h3.textContent = weather.humidity;                                  
+        
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      p.textContent = weather.summary;
 
       container.appendChild(card);
       card.appendChild(h1);
+      card.appendChild(h2);
+      card.appendChild(h3);
       card.appendChild(p);
     });
+    
   } else {
     const errorMessage = document.createElement('marquee');
     errorMessage.textContent = `Gah, it's not working!`;
